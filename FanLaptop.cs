b9568cfe7955560a04a33001ade698b99439bfde
yudhapakel <yudahp23@gmail.com>
2025-03-11 07:23:49 +0700

    menambahkan class KodeProduk

---
+++ FanLaptop.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOD_4
+{
+    internal class FanLaptop
+    {
+    }
+}
---
+++ MainProduk.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using MOD_4;
+
+class MainKodePos
+{
+    static void Main()
+    {
+        Console.Write("Masukkan nama barang: ");
+        string produk = Console.ReadLine();
+        Console.WriteLine($" Barang {produk}, Kode Produk : {KodeProduk.GetKodeProduk(produk)}");
+
+        Console.WriteLine("\n---\n");
+    }
+}
