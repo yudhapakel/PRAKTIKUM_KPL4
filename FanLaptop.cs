0cff67f8d392e6088ab151afa39af52eb244a61b
yudhapakel <yudahp23@gmail.com>
2025-03-11 08:09:03 +0700

    menambahkan class FanLaptop

--- FanLaptop.cs
+++ FanLaptop.cs
@@ -8,5 +8,31 @@ namespace MOD_4
 {
     internal class FanLaptop
     {
+        private enum State {Quiet, Balanced, Peformanced, Turbo }
+        private State stateSekarang;
+
+        public FanLaptop() 
+        {
+            stateSekarang = State.Quiet;
+            Console.WriteLine("fan mode quiet");
+        }
+
+        public void Balanced()
+        {
+            stateSekarang = State.Balanced;
+            Console.WriteLine("Fan Quiet berubah menjadi Balanced");
+        }
+
+        public void Peformanced() 
+        {
+            stateSekarang = State.Peformanced;
+            Console.WriteLine("Fan Quiet berubah menjadi Peformanced");
+        }
+
+        public void Turbo()
+        {
+            stateSekarang = State.Turbo;
+            Console.WriteLine("Fan Quiet berubah menjadi Turbo");
+        }
     }
 }
